2e9d70f106c6f8bc11f3f423ef37fcfda778ba38
abdulalimrakib <abdulalimrakib04@gmail.com>
2023-07-28 02:24:00 +0600

    header complite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '10rem',
+    },
+
+  },
+  plugins: [],
+}
+
