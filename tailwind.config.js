bdcb2a0d14fe9a2fc8884c37eda84d61980b8537
abdulalimrakib <abdulalimrakib04@gmail.com>
2023-07-31 22:51:31 +0600

    conteiner updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {},
     container: {
       center: true,
-      padding: '10rem',
+      DEFAULT: '1rem',
+      sm: '2rem',
+      lg: '4rem',
+      xl: '5rem',
+      '2xl': '6rem',
     },
 
   },
